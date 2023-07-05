c5789fd74dd53a1459d8c358af8c72e6285d7f0e
andre83411 <andreamontes.rei@gmail.com>
2023-07-05 15:33:25 -0600

    first commit

---
+++ http_server.test.js
@@ -0,0 +1,19 @@
+const supertest = require('supertest');
+const app       = require('./http_server');
+const request   = supertest(app);
+
+
+
+it('Hello World!', async done =>{
+    expect(1).toBe(1);
+    done();
+})
+
+var server = app.listen(3000, function(){
+    console.log('Running on port 3000');
+});
+
+afterAll(done => {
+    server.close();
+    done();
+})
